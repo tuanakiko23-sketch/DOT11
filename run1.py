7d0d65ce573ebe00618223f955339233050a314e
Nguyễn Thành Tuấn <133647354+thanhtuanxzx@users.noreply.github.com>
2025-10-22 16:15:43 +0700

    fix

--- run1.py
+++ run1.py
@@ -3,7 +3,7 @@ import threading
 import time
 from concurrent.futures import ThreadPoolExecutor
 
-URL = "https://element-trac-new-1.onrender.com/api/users"  # THAY ĐỔI URL
+URL = "https://camo.githubusercontent.com/e5d95c97160bf769ddc31bf764e26bf7f75a250f68cec01cc9f3905328bd4509/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d7468616e687475616e787a78266c6162656c3d50726f66696c65253230766965777326636f6c6f723d306537356236267374796c653d666c6174"  # THAY ĐỔI URL
 REQUESTS = 10000
 CONCURRENCY = 10000
 
